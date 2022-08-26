9b9994669b6f116605d8ba35b666badc1412aee4
SUDO <82466939+ItsMeSudo@users.noreply.github.com>
2022-08-26 13:52:06 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@ cd /root && \
     apt install unzip wget git curl ruby ruby-dev libmagickwand-dev gcc g++ make -y && \ 
     cd /root/DreamcloudDocker-Ruby && \
     mkdir -p /tmp/sudo && \
+    gem install bundler --no-force && \
     gem install discordrb && \
     gem install dotenv && \
-    gem install ruby_figlet && \
+    gem install ruby_figlet --no-document && \
     sed -i 's/\r//' /root/DreamcloudDocker-Ruby/container-start && \
     sed -i 's/\r//' /root/DreamcloudDocker-Ruby/gitclone && \
     sed -i 's/\r//' /root/DreamcloudDocker-Ruby/setservertoken && \
